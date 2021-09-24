d0c97ed0be8bcaeb84ba687065c4472a15daa438
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-09-25 02:04:20 +0300

    finish_demo_day2

---
+++ demo-day2/src/app.js
@@ -0,0 +1,13 @@
+import Age from './components/age'
+import Counter from './components/counter'
+
+function App() {
+    return (
+        <>
+            <Age age="15" />
+            <Counter />
+        </>
+    );
+}
+
+export default App;
